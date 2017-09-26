02f5c1d37e474284d7c7657912fcf7153aff921d
prudvigali <prudvi.gali@gmail.com>
2017-09-25 21:18:30 -0500

    Initial code

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+
+io.on('connection', function(client){
+    console.log('connection');
+    client.on('create', function (room) {
+        console.log('Join the room');
+        client.join(room);
+        client.in(room).emit('event', 'new user');
+    });
+    client.on('disconnect', function(){
+        console.log("Disconnected....");
+    });
+});
+
+
+server.listen(3000);
